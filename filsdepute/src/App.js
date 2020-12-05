49206c9ce3f5cdb0090a6d967e4b3403af32df29
William Boutet <1734102@cegeplimoilou.ca>
2020-12-05 17:17:00 -0500

    Fils de vieux chien

---
+++ filsdepute/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <b>Arthur est le plus grand fils de pute que la terre aie connu</b>
+      </header>
+    </div>
+  );
+}
+
+export default App;
