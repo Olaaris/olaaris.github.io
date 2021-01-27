a6459e9d40552913e94055c8a0a4b0d1b167d973
William Boutet <1734102@cegeplimoilou.ca>
2021-01-27 01:31:40 -0500

    Contact

--- filsdepute/src/components/FAQ.js
+++ filsdepute/src/components/FAQ.js
@@ -14,7 +14,8 @@ class FAQ extends React.Component{
       <h1>FAQ</h1>
         <b>Comment pouvons-nous attraper ces dangeureux criminels ?</b>
         <div>Ces hommes aiment la <Moula/> et feront tout pour la <Moula/>, il est recommandé de les appâter en utilisant la <Moula/> et d'appeller la police par la suite</div>
-
+      <h1>Contact</h1>
+      <a>will.boutet@gmail.com</a>
     </div>
     );
   }
