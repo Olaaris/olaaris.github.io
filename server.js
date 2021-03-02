bc019c37206eb7c6184b2baba5ed666a3ed1b43a
William Boutet <will.boutet@gmail.com>
2021-03-01 19:16:14 -0500

    CORS

--- server.js
+++ server.js
@@ -10,7 +10,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({limit: '10mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
-app.use(cors());
+
+var allowedOrigins = ['http://localhost:3000',
+                      'https://filsdepute.ca'];
+app.use(cors({
+  origin: function(origin, callback){    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }    return callback(null, true);
+  }
+}));
 
 
 var key = fs.readFileSync(__dirname + '/selfsigned.key');
