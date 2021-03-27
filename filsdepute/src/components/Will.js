82b83da509e6562bed53cca07a0086809e019a30
William Boutet <will.boutet@gmail.com>
2021-03-26 22:09:46 -0400

    Update moi

--- filsdepute/src/components/Will.js
+++ filsdepute/src/components/Will.js
@@ -1,37 +1,17 @@
 import React from 'react';
-import photo from './photos/will.jpg';
-import useSound from 'use-sound';
-import glitch from './sounds/glitch.mp3';
+import photo from './photos/will.png';
 
-class Vincent extends React.Component{
-    componentDidMount = () => {
-        this.audio = new Audio(glitch)
-        this.audio.load()
-        this.playAudio()
-    }
-    playAudio() {
-        const audioPromise = this.audio.play()
-        if (audioPromise !== undefined) {
-          audioPromise
-            .then(_ => {
-              // autoplay started
-            })
-            .catch(err => {
-              // catch dom exception
-              console.info(err)
-            })
-        }
-    }
+class Will extends React.Component{
   render() {
     return (
-    <div className="Vincent">
+    <div>
       <br></br>
 
-      <h5>ɮéռɨ ֆօɨȶ ɖɨɛʊ, ʟɛ քèʀɛ ɖɛ ʝéֆʊֆ, ʟɛ ƈɦʀɨֆȶ, ռօȶʀɛ ֆɛɨɢռɛʊʀ, զʊɨ ռօʊֆ ǟ ɮéռɨ ɛȶ ƈօʍɮʟé ɖɛֆ ɮéռéɖɨƈȶɨօռֆ, ɖɛ ʟ'ɛֆքʀɨȶ, ǟʊ ƈɨɛʟ, ɖǟռֆ ʟɛ ƈɦʀɨֆȶ</h5>
+      <h5>Créateur et chef de la filsdeputerie</h5>
       <br></br>
       <img src={photo}></img>
       <div></div>
-      <b>W̴͙͕̟͇̅̓̿͋̊͊i̴̡̢̨̧̮̟͈̻͖̳͚͙̩̰̮̖̲̝̅̃͗͒̂͑̽̈́̃͌̾̉̂̋͐́͋̕̚͘̚͝l̸̢̝̻̙̤̩̳̖͇͙̤͔͉̹̭̬̔̍̔͗̓̓̊̓͂̍̕l̵̢̗̦̪̘̫̭͙̗̟̮̻̻̀͂̐̍̈́͌̀̍͝͝ͅi̷̡̛̛̲͇͈̣̦̠̞̲̰̾̈̔́̑̾͗͌͘a̵̡̡̢̧͙̖̝̜̼̰̱̬͎̅̋̈́͘͜͜m̷̨̧̢̧̡͇̤̠̖̙͚̣̬̻͈͉͎͉̞͍͂̀͊̅̋͂̄̓̇̃́̇͌̎̍̿̏̕͘</b>
+      <b>William</b>
     </div>
     );
   }
@@ -39,4 +19,4 @@ class Vincent extends React.Component{
 
 }
   
-  export default Vincent;
+  export default Will;
